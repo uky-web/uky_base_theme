371eb4d9a9b64b5496b8d6a3db8e6936da0ab700
GitHub <action@github.com>
2024-02-02 19:05:17 +0000

    January sprint 1 (#126)

---
+++ components/menu/megamenu.js
@@ -0,0 +1,218 @@
+const setUpHeader = () => {
+  /*
+   *   This content is licensed according to the W3C Software License at
+   *   https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document
+   *
+   *   Supplemental JS for the disclosure menu keyboard behavior
+   */
+  const DisclosureNav = function (domNode) {
+    this.rootNode = domNode;
+    this.triggerNodes = [];
+    this.controlledNodes = [];
+    this.openIndex = null;
+    this.useArrowKeys = true;
+  };
+
+  DisclosureNav.prototype.init = function () {
+    const buttons = this.rootNode.querySelectorAll(".main-nav-dropdown-button");
+    for (let i = 0; i < buttons.length; i += 1) {
+      const button = buttons[i];
+      // set selector to the element that contains the content panel
+      const menu = button.parentNode.querySelector("section");
+      this.triggerNodes.push(button);
+      if (menu) {
+        // save ref to button and controlled menu
+        this.controlledNodes.push(menu);
+        // collapse menus
+        button.setAttribute("aria-expanded", "false");
+        // attach event listeners
+        menu.addEventListener("keydown", this.handleMenuKeyDown.bind(this));
+      }
+      button.addEventListener("click", this.handleButtonClick.bind(this));
+      button.addEventListener("keydown", this.handleButtonKeyDown.bind(this));
+    }
+
+    const panelButtons = this.rootNode.querySelectorAll(".sub-nav-panel-button");
+    for (let i = 0; i < panelButtons.length; i += 1) {
+      const button = panelButtons[i];
+      // set selector to the element that contains the content panel
+      const menu = button.parentNode.querySelector("section");
+      this.triggerNodes.push(button);
+      if (menu) {
+        // save ref to button and controlled menu
+        this.controlledNodes.push(menu);
+        // collapse menus
+        button.setAttribute("aria-expanded", "false");
+        // attach event listeners
+        menu.addEventListener("keydown", this.handleMenuKeyDown.bind(this));
+      }
+      button.addEventListener("click", this.handleButtonClick.bind(this));
+      button.addEventListener("keydown", this.handleButtonKeyDown.bind(this));
+    }
+
+    const backButtons = this.rootNode.querySelectorAll("a.close-panel");
+    for (let i = 0; i < backButtons.length; i += 1) {
+      const backButton = backButtons[i];
+      backButton.addEventListener("click", this.handleBackButton.bind(this));
+    }
+
+    /* Close content panel if focus moves from a link */
+    this.rootNode.addEventListener("focusout", this.handleBlur.bind(this));
+  };
+
+  DisclosureNav.prototype.toggleExpand = function (index, expanded) {
+    // close open menu, if applicable
+    if (this.triggerNodes[index]) {
+      if (this.triggerNodes[index].className !== "sub-nav-panel-button") {
+        if (this.openIndex !== index) {
+          this.toggleExpand(this.openIndex, false);
+        }
+      }
+    } else {
+      if (this.openIndex !== index) {
+        this.toggleExpand(this.openIndex, false);
+      }
+    }
+
+    // handle menu at called index
+    if (this.triggerNodes[index]) {
+      this.openIndex = expanded ? index : null;
+      this.triggerNodes[index].setAttribute("aria-expanded", expanded);
+    }
+  };
+
+  DisclosureNav.prototype.controlFocusByKey = function (keyboardEvent, nodeList, currentIndex) {
+    switch (keyboardEvent.key) {
+      case "ArrowUp":
+      case "ArrowLeft":
+        keyboardEvent.preventDefault();
+        if (currentIndex > -1) {
+          const prevIndex = Math.max(0, currentIndex - 1);
+          nodeList[prevIndex].focus();
+        }
+        break;
+      case "ArrowDown":
+      case "ArrowRight":
+        keyboardEvent.preventDefault();
+        if (currentIndex > -1) {
+          const nextIndex = Math.min(nodeList.length - 1, currentIndex + 1);
+          nodeList[nextIndex].focus();
+        }
+
+        break;
+      case "Home":
+        keyboardEvent.preventDefault();
+        nodeList[0].focus();
+        break;
+      case "End":
+        keyboardEvent.preventDefault();
+        nodeList[nodeList.length - 1].focus();
+        break;
+      default:
+      // do nothing
+    }
+  };
+
+  /* Event Handlers */
+  DisclosureNav.prototype.handleBlur = function (event) {
+    const menuContainsFocus = this.rootNode.contains(event.relatedTarget);
+    if (!menuContainsFocus && this.openIndex !== null) {
+      this.toggleExpand(this.openIndex, false);
+    }
+  };
+
+  DisclosureNav.prototype.handleButtonKeyDown = function (event) {
+    const targetButtonIndex = this.triggerNodes.indexOf(document.activeElement);
+
+    // close on escape
+    if (event.key === "Escape") {
+      this.toggleExpand(this.openIndex, false);
+    }
+
+    // move focus into the open menu if the current menu is open
+    else if (this.useArrowKeys && this.openIndex === targetButtonIndex && event.key === "ArrowDown") {
+      event.preventDefault();
+      this.controlledNodes[this.openIndex].querySelector("a").focus();
+    }
+
+    // handle arrow key navigation between top-level buttons, if set
+    else if (this.useArrowKeys) {
+      this.controlFocusByKey(event, this.triggerNodes, targetButtonIndex);
+    }
+  };
+
+  DisclosureNav.prototype.handleButtonClick = function (event) {
+    const button = event.target;
+    const buttonIndex = this.triggerNodes.indexOf(button);
+    const buttonExpanded = button.getAttribute("aria-expanded") === "true";
+    this.toggleExpand(buttonIndex, !buttonExpanded);
+  };
+
+  DisclosureNav.prototype.handleBackButton = function (event) {
+    const panelControl = event.target.parentNode.previousElementSibling;
+    panelControl.setAttribute("aria-expanded", "false");
+  };
+
+  DisclosureNav.prototype.handleMenuKeyDown = function (event) {
+    if (this.openIndex === null) {
+      return;
+    }
+
+    const menuLinks = Array.prototype.slice.call(this.controlledNodes[this.openIndex].querySelectorAll("a"));
+    const currentIndex = menuLinks.indexOf(document.activeElement);
+
+    // close on escape
+    if (event.key === "Escape") {
+      this.triggerNodes[this.openIndex].focus();
+      this.toggleExpand(this.openIndex, false);
+    }
+
+    // handle arrow key navigation within menu links, if set
+    else if (this.useArrowKeys) {
+      this.controlFocusByKey(event, menuLinks, currentIndex);
+    }
+  };
+
+  // switch on/off arrow key navigation
+  DisclosureNav.prototype.updateKeyControls = function (useArrowKeys) {
+    this.useArrowKeys = useArrowKeys;
+  };
+
+  /* Initialize Disclosure Menus */
+  window.addEventListener(
+    "load",
+    function (event) {
+      const menus = document.querySelectorAll(".menu-level-0");
+      const disclosureMenus = [];
+
+      for (let i = 0; i < menus.length; i += 1) {
+        disclosureMenus[i] = new DisclosureNav(menus[i]);
+        disclosureMenus[i].init();
+      }
+
+      return event;
+    },
+    false
+  );
+  // Set a negative left margin on the content panel background
+  // Match the size to the left margin of the slab__wrapper element
+  function setLeftMargin() {
+    let marginLeft = $("#MainHeader .slab__wrapper").css("margin-left");
+    marginLeft = parseFloat(marginLeft, 10);
+    if (marginLeft < 25) {
+      marginLeft = marginLeft + 16;
+    }
+    const newLeft = "-" + marginLeft + "px";
+    const windowWidth = $("#MainHeader").css("width");
+    $(".panel-bg").css({ left: newLeft, width: windowWidth });
+  }
+  // Set the margin when the header is laoded
+  setLeftMargin();
+  // Reset the margin when the viewport is resized
+  window.addEventListener("resize", setLeftMargin);
+};
+
+if (!window.STORYBOOK_ENV) setUpHeader();
+
+/* Export the script for use in Storybook */
+// export default setUpHeader;
---
+++ components/menu/menu.stories.js
@@ -0,0 +1,630 @@
+export default {
+  title: "Menus",
+};
+
+const { image } = import("../image/image.stories");
+
+const items = [
+  {
+    title: "Find & Borrow",
+    url: "",
+    entity: {
+      field_featured: {
+        value: 0,
+      },
+      field_extra_heading: {
+        value: "Stay fueled while you work",
+      },
+      field_extra_background: {
+        value: "radial",
+      },
+      field_extra_links: {
+        0: {
+          title: "Food & Drink in Library Spaces",
+          url: "#",
+        },
+        1: {
+          title: "More Options",
+          url: "#",
+        },
+        2: {
+          title: "",
+          url: "",
+        },
+      },
+    },
+    original_link: {
+      description: "",
+    },
+    below: [
+      {
+        title: "Level 2 Link",
+        url: "#",
+        entity: {
+          field_featured: {
+            value: 1,
+          },
+          field_extra_heading: {
+            value: "",
+          },
+          field_extra_background: {
+            value: "",
+          },
+          field_extra_links: {},
+        },
+        original_link: {
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        },
+      },
+      {
+        title: "Level 2 Link",
+        url: "#",
+        entity: {
+          field_featured: {
+            value: 0,
+          },
+          field_extra_heading: {
+            value: "",
+          },
+          field_extra_background: {
+            value: "",
+          },
+          field_extra_links: {},
+        },
+        original_link: {
+          description: "",
+        },
+      },
+      {
+        title: "Level 2 Link",
+        url: "#",
+        entity: {
+          field_featured: {
+            value: 0,
+          },
+          field_extra_heading: {
+            value: "",
+          },
+          field_extra_background: {
+            value: "",
+          },
+          field_extra_links: {},
+        },
+        original_link: {
+          description: "",
+        },
+      },
+      {
+        title: "Level 2 Link",
+        url: "#",
+        entity: {
+          field_featured: {
+            value: 0,
+          },
+          field_extra_heading: {
+            value: "",
+          },
+          field_extra_background: {
+            value: "",
+          },
+          field_extra_links: {},
+        },
+        original_link: {
+          description: "",
+        },
+      },
+      {
+        title: "Level 2 Link",
+        url: "#",
+        entity: {
+          field_featured: {
+            value: 0,
+          },
+          field_extra_heading: {
+            value: "",
+          },
+          field_extra_background: {
+            value: "",
+          },
+          field_extra_links: {},
+        },
+        original_link: {
+          description: "",
+        },
+      },
+      {
+        title: "Level 2 Link",
+        url: "#",
+        entity: {
+          field_featured: {
+            value: 0,
+          },
+          field_extra_heading: {
+            value: "",
+          },
+          field_extra_background: {
+            value: "",
+          },
+          field_extra_links: {},
+        },
+        original_link: {
+          description: "",
+        },
+      },
+      {
+        title: "Level 2 Link",
+        url: "#",
+        entity: {
+          field_featured: {
+            value: 0,
+          },
+          field_extra_heading: {
+            value: "",
+          },
+          field_extra_background: {
+            value: "",
+          },
+          field_extra_links: {},
+        },
+        original_link: {
+          description: "",
+        },
+      },
+      {
+        title: "Level 2 Link",
+        url: "#",
+        entity: {
+          field_featured: {
+            value: 0,
+          },
+          field_extra_heading: {
+            value: "",
+          },
+          field_extra_background: {
+            value: "",
+          },
+          field_extra_links: {},
+        },
+        original_link: {
+          description: "",
+        },
+      },
+      {
+        title: "Level 2 Link",
+        url: "#",
+        entity: {
+          field_featured: {
+            value: 0,
+          },
+          field_extra_heading: {
+            value: "",
+          },
+          field_extra_background: {
+            value: "",
+          },
+          field_extra_links: {},
+        },
+        original_link: {
+          description: "",
+        },
+      },
+    ],
+  },
+  {
+    title: "Spaces & Technology",
+    url: "",
+    entity: {
+      field_featured: {
+        value: 0,
+      },
+      field_extra_heading: {
+        value: "Stay fueled while you work",
+      },
+      field_extra_background: {
+        value: "splash",
+      },
+      field_extra_links: {},
+    },
+    original_link: {
+      description: "",
+    },
+    below: [
+      {
+        title: "Level 2 Link",
+        url: "#",
+        entity: {
+          field_featured: {
+            value: 1,
+          },
+          field_extra_heading: {
+            value: "",
+          },
+          field_extra_background: {
+            value: "",
+          },
+          field_extra_links: {},
+        },
+        original_link: {
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        },
+      },
+      {
+        title: "Level 2 Link",
+        url: "#",
+        entity: {
+          field_featured: {
+            value: 1,
+          },
+          field_extra_heading: {
+            value: "",
+          },
+          field_extra_background: {
+            value: "",
+          },
+          field_extra_links: {},
+        },
+        original_link: {
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        },
+      },
+      {
+        title: "Level 2 Link",
+        url: "#",
+        entity: {
+          field_featured: {
+            value: 0,
+          },
+          field_extra_heading: {
+            value: "",
+          },
+          field_extra_background: {
+            value: "",
+          },
+          field_extra_links: {},
+        },
+        original_link: {
+          description: "",
+        },
+      },
+      {
+        title: "Level 2 Link",
+        url: "#",
+        entity: {
+          field_featured: {
+            value: 0,
+          },
+          field_extra_heading: {
+            value: "",
+          },
+          field_extra_background: {
+            value: "",
+          },
+          field_extra_links: {},
+        },
+        original_link: {
+          description: "",
+        },
+      },
+      {
+        title: "Level 2 Link",
+        url: "#",
+        entity: {
+          field_featured: {
+            value: 0,
+          },
+          field_extra_heading: {
+            value: "",
+          },
+          field_extra_background: {
+            value: "",
+          },
+          field_extra_links: {},
+        },
+        original_link: {
+          description: "",
+        },
+      },
+      {
+        title: "Level 2 Link",
+        url: "#",
+        entity: {
+          field_featured: {
+            value: 0,
+          },
+          field_extra_heading: {
+            value: "",
+          },
+          field_extra_background: {
+            value: "",
+          },
+          field_extra_links: {},
+        },
+        original_link: {
+          description: "",
+        },
+      },
+    ],
+  },
+  {
+    title: "Research & Scholarship",
+    url: "",
+    entity: {
+      field_featured: {
+        value: 0,
+      },
+      field_extra_heading: {
+        value: "",
+      },
+      field_extra_background: {
+        value: "",
+      },
+      field_extra_links: {},
+    },
+    original_link: {
+      description: "",
+    },
+    below: [
+      {
+        title: "Level 2 Link",
+        url: "#",
+        entity: {
+          field_featured: {
+            value: 1,
+          },
+          field_extra_heading: {
+            value: "",
+          },
+          field_extra_background: {
+            value: "",
+          },
+          field_extra_links: {},
+        },
+        original_link: {
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        },
+      },
+      {
+        title: "Level 2 Link",
+        url: "#",
+        entity: {
+          field_featured: {
+            value: 0,
+          },
+          field_extra_heading: {
+            value: "",
+          },
+          field_extra_background: {
+            value: "",
+          },
+          field_extra_links: {},
+        },
+        original_link: {
+          description: "",
+        },
+      },
+      {
+        title: "Level 2 Link",
+        url: "#",
+        entity: {
+          field_featured: {
+            value: 0,
+          },
+          field_extra_heading: {
+            value: "",
+          },
+          field_extra_background: {
+            value: "",
+          },
+          field_extra_links: {},
+        },
+        original_link: {
+          description: "",
+        },
+      },
+      {
+        title: "Level 2 Link",
+        url: "#",
+        entity: {
+          field_featured: {
+            value: 0,
+          },
+          field_extra_heading: {
+            value: "",
+          },
+          field_extra_background: {
+            value: "",
+          },
+          field_extra_links: {},
+        },
+        original_link: {
+          description: "",
+        },
+      },
+    ],
+  },
+  {
+    title: "Teaching Support",
+    url: "#",
+    entity: {
+      field_featured: {
+        value: 0,
+      },
+      field_extra_heading: {
+        value: "Stay fueled while you work",
+      },
+      field_extra_background: {
+        value: "squares",
+      },
+      field_extra_links: {},
+    },
+    original_link: {
+      description: "",
+    },
+    below: [
+      {
+        title: "Level 2 Link",
+        url: "#",
+        entity: {
+          field_featured: {
+            value: 0,
+          },
+          field_extra_heading: {
+            value: "",
+          },
+          field_extra_background: {
+            value: "",
+          },
+          field_extra_links: {},
+        },
+        original_link: {
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        },
+      },
+      {
+        title: "Level 2 Link",
+        url: "#",
+        entity: {
+          field_featured: {
+            value: 0,
+            field_extra_heading: {
+              value: "",
+            },
+            field_extra_background: {
+              value: "",
+            },
+            field_extra_links: {},
+          },
+        },
+        original_link: {
+          description: "",
+        },
+      },
+      {
+        title: "Level 2 Link",
+        url: "#",
+        entity: {
+          field_featured: {
+            value: 0,
+          },
+          field_extra_heading: {
+            value: "",
+          },
+          field_extra_background: {
+            value: "",
+          },
+          field_extra_links: {},
+        },
+        original_link: {
+          description: "",
+        },
+      },
+      {
+        title: "Level 2 Link",
+        url: "#",
+        entity: {
+          field_featured: {
+            value: 0,
+          },
+          field_extra_heading: {
+            value: "",
+          },
+          field_extra_background: {
+            value: "",
+          },
+          field_extra_links: {},
+        },
+        original_link: {
+          description: "",
+        },
+      },
+      {
+        title: "Level 2 Link",
+        url: "#",
+        entity: {
+          field_featured: {
+            value: 0,
+          },
+          field_extra_heading: {
+            value: "",
+          },
+          field_extra_background: {
+            value: "",
+          },
+          field_extra_links: {},
+        },
+        original_link: {
+          description: "",
+        },
+      },
+      {
+        title: "Level 2 Link",
+        url: "#",
+        entity: {
+          field_featured: {
+            value: 0,
+          },
+        },
+        original_link: {
+          description: "",
+        },
+      },
+    ],
+  },
+  {
+    title: "People",
+    url: "#",
+    entity: {
+      field_featured: {
+        value: 0,
+        field_extra_heading: {
+          value: "",
+        },
+        field_extra_background: {
+          value: "",
+        },
+        field_extra_links: {},
+      },
+    },
+    original_link: {
+      description: "",
+    },
+    below: [],
+  },
+];
+
+import megamenu_template from "./megamenu.twig";
+import megamenu_mobile_template from "./megamenu-mobile.twig";
+
+export const megamenu = (args) =>
+  megamenu_template({
+    items,
+    ...args,
+  });
+// Wrap the single element in the component markup
+megamenu.decorators = [
+  (story) => /* html */ `
+    <header id="MainHeader" class="global-header ">
+      <div class="slab global-header__nav-bar">
+        <div class="slab__wrapper">
+          <div class="global-nav region region-top-nav">
+            <nav id="block-uk-libraries-uky-main-menu" class="contextual-region block-uk-libraries-uky-main-menu block block-menu navigation menu--main" role="navigation" aria-labeledby="block-uk-libraries-uky-main-menu-main-navigation">
+              <div id="block-uk-libraries-uky-main-menu-main-navigation" class="block-title show-for-sr">Main navigation</div>
+              ${story()}
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+  `,
+];
+
+export const megamenu_mobile = (args) =>
+  megamenu_mobile_template({
+    items,
+    ...args,
+  });
